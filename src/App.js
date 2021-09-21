b30f1b5df6b1a21addf736988818e8e25ccc68fc
riduanul <riduanul.haque1@gmail.com>
2021-09-21 21:33:29 +0600

    context code pused

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import ApartmentDetails from "./Components/ApartmentDetails/ApartmentDetails/ApartmentDetails";
 import AddRentHouse from "./Components/Booking/AddRentHouse/AddRentHouse";
 import BookingList from "./Components/Booking/BookingList/BookingList";
 import MyRent from "./Components/Booking/MyRent/MyRent";
-import Home from './Components/Home/Home/Home';
+import Home from "./Components/Home/Home/Home";
 import CreateAccount from "./Components/Login/CreateAccount/CreateAccount";
-import Login from './Components/Login/Login.js';
+import Login from "./Components/Login/Login.js";
+import AuthProvider from "./Context/AuthContext";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Switch>
-         
-         <Route path="/details/:title">
-           <ApartmentDetails/>
-         </Route>
-         <Route path="/login">
-           <Login/>
-         </Route>
-         <Route path="/createAccount">
-           <CreateAccount/>
-         </Route>
-         <Route path="/bookinglist">
-           <BookingList/>
-         </Route>
-         <Route path="/myrent">
-           <MyRent/>
-         </Route>
-         <Route path="/addrent">
-           <AddRentHouse/>
-         </Route>
-         <Route axect path="/" >
-           <Home/>
-       </Route>
-         
-       </Switch>
-     </Router>
-     </div>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/details/:title">
+              <ApartmentDetails />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/createAccount">
+              <CreateAccount />
+            </Route>
+            <Route path="/bookinglist">
+              <BookingList />
+            </Route>
+            <Route path="/myrent">
+              <MyRent />
+            </Route>
+            <Route path="/addrent">
+              <AddRentHouse />
+            </Route>
+            <Route axect path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -1,45 +1,72 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import './Navbar.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-       <div classNameName="container">
-           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <Link to="/" className="navbar-brand" href="#">Apartment Rent</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active"  href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Service</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Content</a>
-        </li>
-       <Link to="booking"> <li className="nav-item">
-          <a className="nav-link" href="#">Dashboard</a>
-        </li></Link>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-       
-      </ul>
-      
-        <Link to="login"><button className="btn btn-outline-success" type="submit">Login</button></Link>
-     
+  return (
+    <div classNameName="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand" href="#">
+            Apartment Rent
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Service
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Content
+                </a>
+              </li>
+              <Link to="/bookinglist">
+                {" "}
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Dashboard
+                  </a>
+                </li>
+              </Link>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+
+            <Link to="login">
+              <button className="btn btn-outline-success" type="submit">
+                Login
+              </button>
+            </Link>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-       </div>
-    )
+  );
 }
