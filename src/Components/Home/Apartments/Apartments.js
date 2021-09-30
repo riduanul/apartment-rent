76288b9d559f997dc40623e2a63ff9a910a89026
riduanul <riduanul.haque1@gmail.com>
2021-10-01 00:55:42 +0600

    some edit on apartments component

--- src/Components/Home/Apartments/Apartments.js
+++ src/Components/Home/Apartments/Apartments.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Apartments.css";
 
 import Apartment from "../Apartment/Apartment";
-import { useEffect } from "react/cjs/react.development";
 
 export default function Apartments() {
   const [info, setInfo] = useState();
